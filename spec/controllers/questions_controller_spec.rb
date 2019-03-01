b2df1a592669827c6ad1c7d32fdf89c7b772adea
Viktor Kachulin <vicart2008@gmail.com>
2019-03-01 18:39:30 +1000

    Add spec for QuestionsController#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    it 'assigns the requested question to @question' do
+      get :show, params: { id: question }
+      expect(assigns(:question)).to eq question
+    end
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
