cab44546c82dffb62573911e9577d67266741eb5
Viktor Kachulin <vicart2008@gmail.com>
2019-03-01 18:44:03 +1000

    Fix spec for QuestionsController#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
     it 'renders show view' do
+      get :show, params: { id: question }
       expect(response).to render_template :show
     end
   end
