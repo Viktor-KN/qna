208cfb4e153a255134cbd71cb552d26dc6006db2
Viktor Kachulin <vicart2008@gmail.com>
2019-03-04 18:36:38 +1000

    Refactor AnswersController

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,11 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[new create]
+
   def new
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.new
   end
 
   def create
-    @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
 
     if @answer.save
@@ -19,5 +19,9 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
   end
 end
