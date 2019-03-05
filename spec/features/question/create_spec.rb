bd4494a172697c5522eb5eb8c4f789efea58bb0a
Viktor Kachulin <vicart2008@gmail.com>
2019-03-05 18:23:32 +1000

    Add feature spec for story 'User can ask a question'

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can ask a question', %q{
+  In order to get help
+  As an user
+  I'd like to be able to ask a question
+} do
+
+
+  scenario 'user tries to ask a question with filled form' do
+    given(:question) { attributes_for(:question) }
+
+    visit new_question_path
+    fill_in 'Title', with: question[:title]
+    fill_in 'Body', with: question[:body]
+    click_on 'Create Question'
+
+    expect(page).to have_content 'Question successfully created'
+    expect(page).to have_content question[:title]
+    expect(page).to have_content question[:body]
+  end
+
+  scenario 'user tries to ask a question with blank form' do
+    visit new_question_path
+    click_on 'Create Question'
+
+    expect(page).to have_content "Title can't be blank"
+  end
+end
