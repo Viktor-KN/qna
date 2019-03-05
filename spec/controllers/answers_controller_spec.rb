0c19643319f7b6fe40f5d35e339e57fafc4d259d
Viktor Kachulin <vicart2008@gmail.com>
2019-03-05 10:50:23 +1000

    Refactor QuestionsController and AnswersController specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,12 +9,15 @@ RSpec.describe AnswersController, type: :controller do
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
+
     it 'assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
+
     it 'assigns @answer.question to be a question' do
       expect(assigns(:answer).question).to eq question
     end
+
     it 'renders new view' do
       expect(response).to render_template :new
     end
@@ -28,15 +31,13 @@ RSpec.describe AnswersController, type: :controller do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
         expect(assigns(:question)).to eq question
       end
-      it 'assigns @answer.question to be a requested question' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        expect(assigns(:answer).question).to eq question
-      end
-      it 'saves a new answer in the database' do
+
+      it 'saves a new answer for question in the database' do
         expect do
           post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        end.to change(Answer, :count).by(1)
+        end.to change(question.answers, :count).by(1)
       end
+
       it 'redirects to @answer.question show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
         expect(response).to redirect_to assigns(:answer).question
@@ -49,6 +50,7 @@ RSpec.describe AnswersController, type: :controller do
           post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
         end.to_not change(Answer, :count)
       end
+
       it 're-renders new view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
         expect(response).to render_template :new
