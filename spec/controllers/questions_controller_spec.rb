650f579c5c74bedcbb0f5666e59c3d32947b8219
Viktor Kachulin <vicart2008@gmail.com>
2019-03-06 18:47:42 +1000

    Fix specs for QuestionsController and AnswersController

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -24,6 +24,14 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'assigns @answer.question to be a question' do
+      expect(assigns(:answer).question).to eq question
+    end
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
