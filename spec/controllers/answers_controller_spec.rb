94371633443b7520807bdd4fb515144b6c8c406d
Viktor Kachulin <vicart2008@gmail.com>
2019-03-06 18:47:42 +1000

    Fix specs for QuestionsController and AnswersController

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,28 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  describe 'GET #new' do
-    let(:question) { create(:question) }
-
-    before { get :new, params: { question_id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'assigns a new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-
-    it 'assigns @answer.question to be a question' do
-      expect(assigns(:answer).question).to eq question
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
   describe 'POST #create' do
     let(:question) { create(:question) }
 
@@ -51,9 +29,9 @@ RSpec.describe AnswersController, type: :controller do
         end.to_not change(Answer, :count)
       end
 
-      it 're-renders new view' do
+      it 're-renders @answer.question show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        expect(response).to render_template :new
+        expect(response).to render_template 'questions/show'
       end
     end
   end
