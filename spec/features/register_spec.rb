0d0bf45360d6456ff8f19b69df1d99b57d0d2ac0
Viktor Kachulin <vicart2008@gmail.com>
2019-03-07 16:51:58 +1000

    Fix feature spec 'User can register'

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -8,33 +8,33 @@ feature 'User can register', %q{
 
   given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
 
-  background { visit register_path }
+  background { visit new_user_registration_path }
 
   scenario 'Unregistered user tries to register' do
 
     fill_in 'Email', with: 'new_user@test.com'
     fill_in 'Password', with: '12345678'
-    fill_in 'Confirm Password', with: '12345678'
-    click_on 'Register'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
 
-    expect(page).to have_content 'Successfully registered.'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
   scenario 'Unregistered user tries to register with existing email' do
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Confirm Password', with: '12345678'
-    click_on 'Register'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: '123456789'
+    fill_in 'Password confirmation', with: '123456789'
+    click_on 'Sign up'
 
-    expect(page).to have_content 'Email already registered'
+    expect(page).to have_content 'Email has already been taken'
   end
 
   scenario 'Unregistered user tries to register with wrong password confirmation' do
     fill_in 'Email', with: 'new_user@test.com'
     fill_in 'Password', with: '12345678'
-    fill_in 'Confirm Password', with: '123456789'
-    click_on 'Register'
+    fill_in 'Password confirmation', with: '123456789'
+    click_on 'Sign up'
 
-    expect(page).to have_content "Passwords doesn't match"
+    expect(page).to have_content "Password confirmation doesn't match Password"
   end
 end
