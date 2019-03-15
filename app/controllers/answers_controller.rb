91df91a849afc5791caa20af47142d0e873f485f
Viktor Kachulin <vicart2008@gmail.com>
2019-03-16 03:05:15 +1000

    Implement feature 'User can edit his answer', make flash messages floating and auto fadeout

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,22 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: %i[create destroy]
+  before_action :authenticate_user!, only: %i[create update destroy]
   before_action :find_question, only: %i[create]
-  before_action :find_answer, only: %i[destroy]
+  before_action :find_answer, only: %i[update destroy]
 
   def create
     @answer = @question.answers.new(answer_params)
     @answer.author = current_user
     if @answer.save
       flash[:notice] = 'Answer successfully created'
+    end
+  end
+
+  def update
+    if current_user.author_of?(@answer)
+      @answer.update(answer_params)
+      flash[:notice] = 'Answer successfully updated'
+    else
+      flash[:alert] = "You don't have permission to do that"
     end
   end
 
