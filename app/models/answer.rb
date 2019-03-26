1b1538401e13afea496171e846273f9775fbbaf2
Viktor Kachulin <vicart2008@gmail.com>
2019-03-26 12:15:07 +1000

    Fix #assign_as_best! method to use #update! method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,7 +9,7 @@ class Answer < ApplicationRecord
   def assign_as_best!
     ActiveRecord::Base.transaction do
       question.answers.update_all(best: false)
-      update(best: true)
+      update!(best: true)
     end
   end
 end
